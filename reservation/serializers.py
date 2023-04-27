45148d3ecd5bd2e669531b914a097d7bd2a1420e
mateuszkuczykkuczynski <mati.kuczykk@gmail.com>
2023-04-27 21:55:44 +0200

    feat/display_all_reserv_from_table

--- reservation/serializers.py
+++ reservation/serializers.py
@@ -2,35 +2,42 @@ from rest_framework import serializers
 from .models import Table, Reservation
 
 
-class ReservationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Reservation
-        fields = '__all__'
-
-
-class TableSerializer(serializers.ModelSerializer):
-    restaurant_name = serializers.SerializerMethodField()
-    reservation = ReservationSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Table
-        fields = ("restaurant_name", "location", "capacity",
-                  "is_reserved", "reservation", )
-
-    def get_restaurant_name(self, obj):
-        return obj.location.name
-
-
 # class TableSerializer(serializers.ModelSerializer):
+#     # reservations = serializers.PrimaryKeyRelatedField(queryset=Reservation.objects.all(), many=True)
+#     reservations = ReservationSerializer(many=True)
 #     restaurant_name = serializers.SerializerMethodField()
 #
 #     class Meta:
 #         model = Table
 #         fields = ("restaurant_name", "location", "capacity",
-#                   "is_reserved", "reservation", )
+#                   "is_reserved", "reservations", )
 #
 #     def get_restaurant_name(self, obj):
 #         return obj.location.name
+
+
+class TableSerializer(serializers.ModelSerializer):
+    restaurant_name = serializers.SerializerMethodField()
+    reservations = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Table
+        fields = ("restaurant_name", "capacity", "reservations")
+
+    def get_restaurant_name(self, obj):
+        return obj.location.name
+
+    def get_reservations(self, obj):
+        reservations = obj.reservations.all()
+        return ReservationSerializer(reservations, many=True).data
+
+
+class ReservationSerializer(serializers.ModelSerializer):
+    table_list = TableSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Reservation
+        fields = '__all__'
 
 
 class ReservationSerializerEditableFields(serializers.ModelSerializer):
@@ -44,3 +51,16 @@ class ReservationDetailsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
         fields = ("reserved_time", "reserved_time_end", "table_number")
+
+
+
+# class TableSerializer(serializers.ModelSerializer):
+#     restaurant_name = serializers.SerializerMethodField()
+#
+#     class Meta:
+#         model = Table
+#         fields = ("restaurant_name", "location", "capacity",
+#                   "is_reserved", "reservation", )
+#
+#     def get_restaurant_name(self, obj):
+#         return obj.location.name
