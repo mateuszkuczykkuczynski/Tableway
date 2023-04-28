15adbab71c233ab04fd8d275aa4651af8c8de7e0
mateuszkuczykkuczynski <mati.kuczykk@gmail.com>
2023-04-28 20:15:20 +0200

    feat/dynamic_schema

--- django_project/urls.py
+++ django_project/urls.py
@@ -16,13 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from drf_spectacular.views import (SpectacularAPIView, SpectacularSwaggerView)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/v1/", include("reservation.urls")),
     re_path(r'^rest-auth/', include('rest_auth.urls')),
-    re_path(r'^rest-auth/registration/', include('rest_auth.registration.urls'))
+    re_path(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
 
     # path("api/v1/dj-rest-auth/", include("dj_rest_auth.urls")),
     # path("api/v1/dj-rest-auth/registration/", include("dj_rest_auth.registration.urls")),
