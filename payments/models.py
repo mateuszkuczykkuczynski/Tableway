d866f4fa3907edeb3abdf885dbb4cb5eb7199aaa
mateuszkuczykkuczynski <mati.kuczykk@gmail.com>
2023-05-08 23:40:43 +0200

    feat/payment_app_updates

--- payments/models.py
+++ payments/models.py
@@ -1,11 +1,9 @@
 from django.db import models
 import random
 
-from reservations.models import Employee, Reservation
-
 
 class Payment(models.Model):
-    reservation = models.ForeignKey(Reservation, on_delete=models.SET_NULL, related_name='payments')
+    reservation = models.ForeignKey('bookings.Reservation', on_delete=models.SET_NULL, related_name='payments', null=True)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
     secret = models.CharField(max_length=1000, default="", blank=True)
     checkout_url = models.CharField(max_length=1000, default="", blank=True)
@@ -14,14 +12,19 @@ class Payment(models.Model):
         self.secret = str(random.randint(10000, 99999))
 
 
-class Tips(models.Model):
-    employee = models.ForeignKey(Employee, on_delete=models.SET_NULL, related_name='received_tips', null=True)
-    reservation = models.ForeignKey(Reservation, on_delete=models.SET_NULL, related_name='tips', null=True)
+class Tip(models.Model):
+    employee = models.ForeignKey('bookings.Employee', on_delete=models.SET_NULL, related_name='received_tips', null=True)
+    reservation = models.ForeignKey('bookings.Reservation', on_delete=models.SET_NULL, related_name='tips', null=True)
     amount = models.DecimalField(max_digits=8, decimal_places=2)
     secret = models.CharField(max_length=1000, default="", blank=True)
     checkout_url = models.CharField(max_length=1000, default="", blank=True)
     received = models.BooleanField(default=False)
-    date = models.DateTimeField()  # TODO - Based on reservation instance!
+    date = models.DateTimeField()  # To test!
 
     def generate_secret(self):
         self.secret = str(random.randint(10000, 99999))
+
+    def save(self, *args, **kwargs):
+        if self.reservation:
+            self.date = self.reservation.reserved_time
+        super().save(*args, **kwargs)
