b26a958ee776a7af5e7c62780f98c216ec2d2677
mateuszkuczykkuczynski <mati.kuczykk@gmail.com>
2023-08-01 23:26:23 +0200

    feat/celery_tasks_modified

---
+++ payments/tasks.py
@@ -0,0 +1,29 @@
+from time import sleep
+from django.core.mail import send_mail
+from celery import shared_task
+from environs import Env
+
+from django_project import celery_app
+
+
+env = Env()
+env.read_env()
+
+
+# Test purpose
+@celery_app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+
+
+@shared_task()
+def send_feedback_email_after_payment_task(email_address, message):
+    """Sends an email when the feedback form has been submitted."""
+    sleep(10)  # Simulate operation time that Django needs synchronously
+    send_mail(
+        "Hi",
+        f"\t{message}\n\nYour payment was successfully retrieved! Thanks for choosing Tableway!",
+        env("EMAIL_ADDRESS"),
+        [email_address],
+        fail_silently=False,
+    )
