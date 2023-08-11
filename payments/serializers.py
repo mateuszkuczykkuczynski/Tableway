70f9ffc2a1393ec8898fd3a28e85d4e5ece263d8
mateuszkuczykkuczynski <mati.kuczykk@gmail.com>
2023-08-11 23:28:03 +0200

    refactor/class_refactoring_and_docs_added_payments_app

--- payments/serializers.py
+++ payments/serializers.py
@@ -5,6 +5,10 @@ from bookings.models import Reservation
 
 
 class CreatePaymentForReservationSerializer(serializers.ModelSerializer):
+    """
+    Serializer for creating a payment associated with a reservation.
+    Validates the amount to ensure it's non-negative and not larger than eight digits.
+    """
     reservation_choice = serializers.PrimaryKeyRelatedField(queryset=Reservation.objects.none())
     amount = serializers.IntegerField()
 
@@ -25,6 +29,9 @@ class CreatePaymentForReservationSerializer(serializers.ModelSerializer):
 
 
 class PaymentsDetailsSerializer(serializers.ModelSerializer):
+    """
+    Serializer to provide details of a payment, including its completion status, associated reservation, and amount.
+    """
 
     class Meta:
         model = Payment
@@ -32,13 +39,19 @@ class PaymentsDetailsSerializer(serializers.ModelSerializer):
 
 
 class CompletePaymentSerializer(serializers.ModelSerializer):
-
+    """
+    Serializer to update the completion status of a payment.
+    """
     class Meta:
         model = Payment
         fields = ("completed",)
 
 
 class TipEmployeeSerializer(serializers.ModelSerializer):
+    """
+    Serializer for tipping an employee.
+    Validates the tip amount to ensure it's non-negative and not larger than eight digits.
+    """
     amount = serializers.IntegerField()
 
     def validate_amount(self, value):
@@ -54,18 +67,27 @@ class TipEmployeeSerializer(serializers.ModelSerializer):
 
 
 class AllUserTipsSerializer(serializers.ModelSerializer):
+    """
+    Serializer to list all tips associated with a user, including details like reservation, amount, date, receipt status, and the employee who received the tip.
+    """
     class Meta:
         model = Tip
         fields = ("reservation", "amount", "date", "received", "employee")
 
 
 class AllEmployeeTipsSerializer(serializers.ModelSerializer):
+    """
+    Serializer to list all tips received by an employee, including details like reservation, amount, date, and receipt status.
+    """
     class Meta:
         model = Tip
         fields = ("reservation", "amount", "date", "received",)
 
 
 class AllRestaurantTipsSerializer(serializers.ModelSerializer):
+    """
+    Serializer to list all tips associated with a restaurant, including details like reservation, amount, date, receipt status, and a secret field.
+    """
     class Meta:
         model = Tip
         fields = ("reservation", "amount", "date", "received", "secret")
