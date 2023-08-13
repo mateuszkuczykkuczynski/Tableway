7652450f56088681e96d03de5c5c9b9b8215cb0a
mateuszkuczykkuczynski <mati.kuczykk@gmail.com>
2023-08-13 22:49:49 +0200

    refactor/class_refactoring_and_docs_added_accounts_app

--- accounts/mixins.py
+++ accounts/mixins.py
@@ -2,9 +2,22 @@ from rest_framework.mixins import UpdateModelMixin
 
 
 class PutOnlyMixin(object):
+    """
+    A mixin that provides functionality for handling PUT requests only.
+
+    Overrides the `update` method to ensure that the update flag is set to True.
+    The `perform_update` method is responsible for saving the serialized data.
+    """
     def update(self, request, *args, **kwargs):
+        """
+        Overrides the update method to set the 'update' flag to True.
+        Calls the `update` method from the UpdateModelMixin.
+        """
         kwargs['update'] = True
         return UpdateModelMixin.update(self, request, *args, **kwargs)
 
     def perform_update(self, serializer):
+        """
+        Saves the serialized data.
+        """
         serializer.save()
